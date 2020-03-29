473c75137b25852d42e75fbb909eadcc9ad7df27
alchern2412 <mynameislyosha2412@yandex.com>
2020-03-29 23:35:08 +0300

    Add sync posts - redux

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,20 @@
 import React from 'react'
-import Post from "./Post";
+import {connect} from 'react-redux'
+import Post from "./Post"
 
-export default ({posts}) => {
-    if (!posts.length) {
+const Posts = ({syncPosts}) => {
+    if (!syncPosts.length) {
         return <p className="text-center">Постов пока нет</p>
     }
-    return posts.map(post => <Post post={post} key={post}/>)
+    return syncPosts.map(post => <Post post={post} key={post.id}/>)
 }
+
+const mapStateToProps = state => {
+    return {
+        syncPosts: state.posts.posts
+    }
+}
+
+
+
+export default connect(mapStateToProps, null)(Posts)
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {rootReducer} from "./redux/rootReducer";
 import {Provider} from 'react-redux'
 import {compose, createStore} from "redux";
+
+import {rootReducer} from "./redux/rootReducer";
+
 
 const store = createStore(rootReducer, compose(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
